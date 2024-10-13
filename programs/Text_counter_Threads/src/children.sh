006403211a286b3875e7b7398b0bf6864241213b
Nico198 <nicolas.sandoval01@alumnos.uach.cl>
2024-10-13 18:33:37 -0300

    modificacion script y correccion de error

--- programs/Text_counter_Threads/src/children.sh
+++ programs/Text_counter_Threads/src/children.sh
@@ -9,6 +9,5 @@
 # Obtenemos el nombre del archivo
 filename=$(basename "$1" "$4")
 id=$(grep -w "$filename" "$3/ID$4" | awk -F '; ' '{print $2}')
-echo "el id de $filename es $id"
 
-tr "[:upper:]" "[:lower:]" < "$1" | tr -cs "[:lower:]" "[\n*]" | grep -vxFf "$5" | sort | uniq -c | sort -nr | awk '{print $1 "; " $2}' | tee -p "$2"/"$id$4" | wc -l | awk -v name="$1" '{print name ": " $1 " palabras"}' 
+tr "[:upper:]" "[:lower:]" < "$1" | tr -cs "[:lower:]" "[\n*]" | grep -vxFf "$5" | sort | grep -v '^$' | uniq -c | sort -nr | awk '{print $1 "; " $2}' | tee -p "$2"/"$id$4" | wc -l | awk -v name="$1" '{print name ": " $1 " palabras"}' 
