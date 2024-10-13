1571afd9df3026f42c04575f651c8f8664c30254
niik4466 <niikk4466@gmail.com>
2024-10-13 18:19:24 -0300

    Arreglado scripts para conteo

--- programs/Text_counter_Threads/src/children.sh
+++ programs/Text_counter_Threads/src/children.sh
@@ -7,7 +7,8 @@
 # Quinto argumento path al archivo stopwords $5
 
 # Obtenemos el nombre del archivo
-filename=$(basename "$1")
-id=$(grep "$filename" "$3/ID$4" | awk -F '; ' '{print $2}')
+filename=$(basename "$1" "$4")
+id=$(grep -w "$filename" "$3/ID$4" | awk -F '; ' '{print $2}')
+echo "el id de $filename es $id"
 
-tr "[:upper:]" "[:lower:]" < "$1" | tr -cs "[:lower:]" "[\n*]" | grep -vxFf "$5" | sort | uniq -c | sort -nr | awk '{print $1 "; " $2}' | tee "$2"/"$id$4" | wc -l | awk -v name="$1" '{print name ": " $1 " palabras"}' 
+tr "[:upper:]" "[:lower:]" < "$1" | tr -cs "[:lower:]" "[\n*]" | grep -vxFf "$5" | sort | uniq -c | sort -nr | awk '{print $1 "; " $2}' | tee -p "$2"/"$id$4" | wc -l | awk -v name="$1" '{print name ": " $1 " palabras"}' 
--- programs/Text_counter_Threads/src/createID.sh
+++ programs/Text_counter_Threads/src/createID.sh
@@ -5,11 +5,9 @@ archivos_dir="$2"
 mapa_archivo="$3"
 
 # Crear las ID's
-{
 count=0
 find "$archivos_dir" -maxdepth 1 -name '*'"$extension" -print0 | while IFS= read -r -d '' archivo; do
-  archivo=$(basename "$archivo")
+  archivo=$(basename "$archivo" "$1")
   echo "$archivo; $count"
   count=$((count+1))
-done
-} > "$mapa_archivo/ID$extension"
+done > "$mapa_archivo/ID$extension"
